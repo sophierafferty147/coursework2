051fc21fdab6d6644c296857c76d681038304bb3
sophierafferty147 <Guest@stk-cs01-ws14.westlothian.sch.uk>
2015-12-15 10:29:03 +0000

    Sprint 2.1

---
+++ CONCERT.java
@@ -0,0 +1,19 @@
+
+
+public class CONCERT
+{
+    // instance variables - replace the example below with your own
+    
+    public CONCERT()
+    {
+        // initialise instance variables
+        
+    }
+    
+    public void processCustomer()
+    {
+        
+    }
+
+
+}
